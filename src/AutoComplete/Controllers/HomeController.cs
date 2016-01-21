7505c706ac817521e08fc3da76711ab8cf221c3f
braincreator <alexxudin@gmail.com>
2016-01-21 17:53:28 +0200

    final demo-1

--- src/AutoComplete/Controllers/HomeController.cs
+++ src/AutoComplete/Controllers/HomeController.cs
@@ -4,29 +4,31 @@ using System.Diagnostics;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using AutoComplete.Models;
-using Google.Apis.Auth.OAuth2;
-using Google.Apis.Services;
 using Microsoft.AspNet.Mvc;
 //using Enyim.Caching;
 //using Enyim.Caching.Configuration;
 //using Enyim.Caching.Memcached;
-using System.Net;
-using Google.Apis.Datastore.v1beta1;
-using Google.Apis.Datastore.v1beta1.Data;
-using Google.Apis.Http;
+using System.Threading;
+//using Google.Apis.Datastore.v1beta1;
+//using Google.Apis.Datastore.v1beta1.Data;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace AutoComplete.Controllers
 {
     public class HomeController : Controller
     {
         AutoCompleteModel model = new AutoCompleteModel();
-        private DatastoreService service;
+        //private DatastoreService service;
         //private MemcachedClient memcachedClient;
+        List<Product> AllProducts = new List<Product>();
 
         public HomeController()
         {
             InitDataStoreService();
             InitMemcachedClient();
+            PrepareDataAsync().ConfigureAwait(false);
+            Debug.WriteLine("Ready");
         }
 
         public IActionResult Index()
@@ -56,30 +58,30 @@ namespace AutoComplete.Controllers
         [HttpPost]
         public JsonResult Autocomplete(string term, int limit)
         {
-            GqlQuery q = new GqlQuery();
-            q.QueryString = $"select * from product where nm >= '{term}' limit {limit}";
-            q.AllowLiteral = true;
+            //GqlQuery q = new GqlQuery();
+            //q.QueryString = $"select * from product where nm >= '{term}' limit {limit}";
+            //q.AllowLiteral = true;
 
-            RunQueryRequest request = new RunQueryRequest();
-            request.GqlQuery = q;
+            //RunQueryRequest request = new RunQueryRequest();
+            //request.GqlQuery = q;
 
-            var obj = service.Datasets.RunQuery(request, "containerdemo-1190");
-            RunQueryResponse resp = null;
-            try
-            {
-                resp = obj.Execute();
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex.Message);
-                throw;
-            }
+            //var obj = service.Datasets.RunQuery(request, "containerdemo-1190");
+            //RunQueryResponse resp = null;
+            //try
+            //{
+            //    resp = obj.Execute();
+            //}
+            //catch (Exception ex)
+            //{
+            //    Debug.WriteLine(ex.Message);
+            //    throw;
+            //}
 
-            var list = resp.Batch.EntityResults.Select(entityResult => new Product
-            {
-                Name = entityResult.Entity.Properties["nm"].Values.First().StringValue,
-                Id = int.Parse(entityResult.Entity.Key.Path.First().Id.ToString())
-            }).ToList();
+            //var list = resp.Batch.EntityResults.Select(entityResult => new Product
+            //{
+            //    Name = entityResult.Entity.Properties["nm"].Values.First().StringValue,
+            //    Id = int.Parse(entityResult.Entity.Key.Path.First().Id.ToString())
+            //}).ToList();
 
 
             ////test memcache
@@ -90,39 +92,39 @@ namespace AutoComplete.Controllers
             //    Debug.WriteLine(cacheItem);
             //}
 
-            //var list = new List<Product>();
-            //for (int i = 0; i < 50; i++)
-            //{
-            //    list.Add(new Product()
-            //    {
-            //        Id = i,
-            //        Name = $"product{i}"
-            //    });
-            //}
-            return Json(list);
+
+            
+            var names = AllProducts.Where(p => p.Name.ToLower().StartsWith(term.ToLower())).Select(p=>p.Name).Distinct().Take(limit).ToList();
+            List<Product> products = new List<Product>();
+            foreach (var name in names)
+            {
+                products.Add(new Product() {Id = names.IndexOf(name), Name = name});
+            }
+            return Json(products);
         }
 
         void InitDataStoreService()
         {
-            const string serviceAccountEmail = "containersvcacc@containerdemo-1190.iam.gserviceaccount.com";
-            var certificate = new X509Certificate2(@"ContainerDemo-65e90699b00f.p12", "notasecret", X509KeyStorageFlags.Exportable);
+            //const string serviceAccountEmail = "containersvcacc@containerdemo-1190.iam.gserviceaccount.com";
+            //var certificate = new X509Certificate2(@"ContainerDemo-65e90699b00f.p12", "notasecret", X509KeyStorageFlags.Exportable);
 
 
-            var credential = new ServiceAccountCredential(
-                   new ServiceAccountCredential.Initializer(serviceAccountEmail)
-                   {
-                       Scopes = new[] {
-                           DatastoreService.Scope.CloudPlatform.ToLower(),
-                           DatastoreService.Scope.Datastore.ToLower(),
-                           DatastoreService.Scope.UserinfoEmail.ToLower()
-                       }
-                   }.FromCertificate(certificate));
-            
-            service = new DatastoreService(new BaseClientService.Initializer
-            {
-                HttpClientInitializer = credential,
-                ApplicationName = "***"
-            });
+            //var credential = new ServiceAccountCredential(
+            //       new ServiceAccountCredential.Initializer(serviceAccountEmail)
+            //       {
+            //           Scopes = new[] {
+            //               "https://www.googleapis.com/auth/cloud-platform",
+            //                "https://www.googleapis.com/auth/datastore",
+            //                "https://www.googleapis.com/auth/userinfo.email"
+            //           }
+            //       }.FromCertificate(certificate));
+
+           
+            //service = new DatastoreService(new BaseClientService.Initializer
+            //{
+            //    HttpClientInitializer = credential,
+            //    ApplicationName = "***"
+            //});
         }
 
         void InitMemcachedClient()
@@ -138,5 +140,17 @@ namespace AutoComplete.Controllers
 
             //memcachedClient = new MemcachedClient(config);
         }
+
+        async Task PrepareDataAsync()
+        {
+            var fileContents = System.IO.File.ReadAllText(@"tracks_per_year.txt");
+            var names = fileContents.Split(new string[] {"<SEP>"}, StringSplitOptions.None);
+            int id = 0;
+            foreach (var name in names)
+            {
+                AllProducts.Add(new Product() { Id = id, Name = name});
+                id++;
+            }
+        } 
     }
 }
--- src/AutoComplete/demosetup.sh
+++ src/AutoComplete/demosetup.sh
@@ -1,13 +1,16 @@
+cd ..
+cd ..
+cd ..
 sudo rm -rf demo/
 sudo git clone https://github.com/braincreator/DockerDemoAutoComplete /demo
 cd /demo/src/AutoComplete
-docker build -t gcr.io/containerdemo-1190/acdemo4 .
-gcloud docker push gcr.io/containerdemo-1190/acdemo4
+docker build -t gcr.io/containerdemo-1190/acdemo .
+gcloud docker push gcr.io/containerdemo-1190/acdemo
 kubectl delete services acdemo
 kubectl get services
 kubectl delete rc acdemo
-kubectl run acdemo4 --image=gcr.io/containerdemo-1190/acdemo4 --port=5005
-kubectl expose rc acdemo4 --type="LoadBalancer"
+kubectl run acdemo --image=gcr.io/containerdemo-1190/acdemo --port=5005
+kubectl expose rc acdemo --type="LoadBalancer"
 sleep 1m
 kubectl get services
 cd ..

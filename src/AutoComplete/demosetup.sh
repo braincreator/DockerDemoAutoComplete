0e8c06c4c020baedcda3a8103b1bdea0e28bafe7
braincreator <alexxudin@gmail.com>
2016-01-21 14:52:35 +0200

    3

--- src/AutoComplete/demosetup.sh
+++ src/AutoComplete/demosetup.sh
@@ -1,11 +1,12 @@
+sudo rm -rf demo/
 sudo git clone https://github.com/braincreator/DockerDemoAutoComplete /demo
 cd /demo/src/AutoComplete
-docker build -t gcr.io/containerdemo-1190/acdemo .
-gcloud docker push gcr.io/containerdemo-1190/acdemo
+docker build -t gcr.io/containerdemo-1190/acdemo2 .
+gcloud docker push gcr.io/containerdemo-1190/acdemo2
 kubectl delete services acdemo
 kubectl get services
 kubectl delete rc acdemo
-kubectl run acdemo --image=gcr.io/containerdemo-1190/acdemo --port=5005
+kubectl run acdemo --image=gcr.io/containerdemo-1190/acdemo2 --port=5005
 kubectl expose rc acdemo --type="LoadBalancer"
 sleep 1m
 kubectl get services
